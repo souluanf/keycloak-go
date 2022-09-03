6e7ad4cd6c699d9951e02909acd9243c53153fab
Luan Fernandes <souluanf@icloud.com>
2022-09-02 22:18:41 -0300

    pegar informações de usuário

--- goclient/main.go
+++ goclient/main.go
@@ -48,10 +48,26 @@ func main() {
 			return
 		}
 
+		idToken, ok := token.Extra("id_token").(string)
+		if !ok {
+			http.Error(writer, "Falha ao gerar o IdToken", http.StatusInternalServerError)
+			return
+		}
+
+		userInfo, err := provider.UserInfo(ctx, oauth2.StaticTokenSource(token))
+		if err != nil {
+			http.Error(writer, "Erro ao pegar userInfo", http.StatusInternalServerError)
+			return
+		}
+
 		resp := struct {
 			AccessToken *oauth2.Token
+			IdToken     string
+			UserInfo    *oidc.UserInfo
 		}{
 			token,
+			idToken,
+			userInfo,
 		}
 		data, err := json.Marshal(resp)
 		if err != nil {
